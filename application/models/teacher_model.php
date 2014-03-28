d74eb0e3a6641c5227bf27040154bd65349f9369
jayArnel <jay.arnel@gmail.com>
2014-03-28 20:48:19 +0800

    dumdum

--- application/models/teacher_model.php
+++ application/models/teacher_model.php
@@ -30,8 +30,8 @@
 			return $query->result();
 		}
 
-		function getHomeroom($teacher_id) {
-			$query = $this->db->query("SELECT  * FROM teacher_sections INNER JOIN sections ON teacher_sections.section_id = sections.id WHERE teacher_id = '$teacher_id'");
+		function getHomeroom($teacher_id, $year_sem_id) {
+			$query = $this->db->query("SELECT  * FROM teacher_sections INNER JOIN sections ON teacher_sections.section_id = sections.id WHERE teacher_id = '$teacher_id' AND year_sem_id = '$year_sem_id'");
 			return $query->result();	
 		}
 
