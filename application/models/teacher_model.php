c819389d9d20debce2d7bafc879fb429980f28ff
jayArnel <jay.arnel@gmail.com>
2014-03-29 05:15:15 +0800

    d

--- application/models/teacher_model.php
+++ application/models/teacher_model.php
@@ -55,6 +55,20 @@
 			return $query->result();
 		}
 
+		function updateGrades($student_id, $subject_id, $year_sem_id, $period, $value){
+			$query = $this->db->query("SELECT *,  student_year.id AS sy_id FROM students INNER JOIN student_year ON student_year.student_id = students.id WHERE student_year.sys_id = '$year_sem_id'");
+			$result = $query->result();
+			$student_year = $result[0]->sy_id;
+			$query = $this->db->query("SELECT *, classes.id = class_id FROM grades INNER JOIN classes ON classes.id = grades.class_id WHERE classes.subject_id = '$subject_id' AND classes.year_sem_id = '$year_sem_id' AND grades.student_year_id = '$student_year' AND grades.period = '$period'");
+			$result = $query->result();
+			$class_id = $result[0]->class_id;
+			if (sizeof($result) == 0) {
+				$this->db->query("INSERT INTO grades(student_year_id, class_id, period, grade) VALUES ('$student_year', '$class_id', '$period', '$grade')");	
+			} else {
+				$this->db->query("UPDATE grades SET grade = '$grade' WHERE student_year_id = '$student_year' AND class_id = '$class_id' AND period = '$period'");	
+			}
+		}
+
 		function updatePces($student_id, $year_id, $trait_id, $period, $grade) {
 			$query = $this->db->query("SELECT * pces WHERE student_id = '$student_id' AND year_id = '$year_id' AND trait_id = '$trait_id' AND period = '$period' AND grade = '$grade'");
 			$result = $query->result();
